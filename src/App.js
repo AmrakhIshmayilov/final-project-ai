4c89a350a5343a8ca8b25a32e32c840e808735a5
xayala86 <xayalaaslan86@gmail.com>
2023-06-15 23:46:34 +0400

    Footer section is completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 //{} Obyektin Parcalanmasi--Object Decomposition
-// import react from 'react';
+//import react from "react";
 import Navbar from "./components/Navbar";
 import Introduction from "./components/Introduction.jsx";
 import TrustedByCompanies from "./components/TrustedByCompanies";
 import Apply from "./components/Apply";
-// import Award from './components/Award';
+//import Award from "./components/Award";
 import AwardsContanier from "./components/AwardsContanier";
 import WhatAboutClients from "./components/WhatAboutClients";
 import Footer from "./components/Footer";
+
 // Komponentlerin daxil edildiyi yer (END) //
 
 // Marshut Komponentleri
